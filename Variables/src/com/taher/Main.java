f593b92e422e1f807d02e791664d26ee405f2d7f
taher <tahertalikoti@gmail.com>
2021-06-13 12:34:16 +0530

    added variables program in java

---
+++ Variables/src/com/taher/Main.java
@@ -0,0 +1,10 @@
+package com.taher;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int num = 10;
+        System.out.println(num);
+    }
+}
